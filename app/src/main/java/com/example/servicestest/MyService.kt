59a4166e956b8f123476dee3268c8532852e5c07
MPapyrus <secenmasterdead@yandex.ru>
2025-02-09 00:02:49 +0500

    Learned how restarting services

--- app/src/main/java/com/example/servicestest/MyService.kt
+++ app/src/main/java/com/example/servicestest/MyService.kt
@@ -23,13 +23,15 @@ class MyService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
 
+        val start = intent?.getIntExtra(EXTRA_START, 0) ?: 0
+
         coroutineScope.launch {
-            for (i in 0 until 100) {
+            for (i in start until 100 + start) {
                 delay(1000)
                 log("Timer: $i")
             }
         }
-        return super.onStartCommand(intent, flags, startId)
+        return START_REDELIVER_INTENT
     }
 
     override fun onDestroy() {
@@ -48,8 +50,12 @@ class MyService : Service() {
 
     companion object {
 
-        fun newIntent(context: Context): Intent {
-            return Intent(context, MyService::class.java)
+        private const val EXTRA_START = "start"
+
+        fun newIntent(context: Context, start: Int): Intent {
+            return Intent(context, MyService::class.java).apply {
+                putExtra(EXTRA_START, start)
+            }
         }
     }
 }
