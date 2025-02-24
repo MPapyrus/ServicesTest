84bb2530fe6feb804f8e17abf130c4c2530fdbcc
MPapyrus <secenmasterdead@yandex.ru>
2025-02-24 15:02:55 +0500

    Created JobIntentService

---
+++ app/src/main/java/com/example/servicestest/MyIntentService2.kt
@@ -0,0 +1,61 @@
+package com.example.servicestest
+
+import android.annotation.SuppressLint
+import android.app.IntentService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyIntentService2 : IntentService(NAME) {
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+
+        setIntentRedelivery(true)
+    }
+
+    override fun onHandleIntent(p0: Intent?) {
+        log("onHandleIntent")
+
+        val page = p0?.getIntExtra(PAGE, 0) ?: 0
+
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyIntentService2: $message")
+    }
+
+    companion object {
+        private const val NAME = "MyIntentService2"
+
+        private const val PAGE = "page"
+
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+}
