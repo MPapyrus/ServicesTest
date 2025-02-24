d96c3763ed073002ed6350db29456197b2e74fc5
MPapyrus <secenmasterdead@yandex.ru>
2025-02-24 18:29:35 +0500

    Created WorkManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.work)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
